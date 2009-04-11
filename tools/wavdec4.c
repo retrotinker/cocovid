2c4b6bf9e67c348bec522a0ba85821b4e5639500
John W. Linville <linville@tuxdriver.com>
2009-04-10 22:04:47 -0400

    Tool updates...
    	-- default to 11025Hz
    	-- accomodate generation of 11025/4 Hz PCM data
    	-- add tool to help w/ building final video file

---
+++ tools/wavdec4.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+unsigned char wavbuf[1024];
+unsigned char outbuf[1024];
+
+void usage(char *prg)
+{
+	printf("Usage: %s infile outfile\n", prg);
+}
+
+int main(int argc, char *argv[])
+{
+	int infd, outfd;
+	int rc, i;
+	int wavidx = 0, outidx = 0;
+	char hdbuf[44];
+
+	if (argc < 3) {
+		usage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	/* open input file */
+	infd = open(argv[1], O_RDONLY);
+
+	/* open output file */
+	outfd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+	if (read(infd, hdbuf, sizeof(hdbuf)) != sizeof(hdbuf))
+		perror("head read");
+
+	while (rc = read(infd, &wavbuf, sizeof(wavbuf)) == sizeof(wavbuf)) {
+		while (wavidx < sizeof(wavbuf)) {
+			outbuf[outidx++] = wavbuf[wavidx];
+			wavidx += 4;
+		}
+		if (outidx == sizeof(outbuf)) {
+			if (write(outfd, outbuf, sizeof(outbuf)) != sizeof(outbuf))
+				perror("pcm write");
+			outidx = 0;
+		} else if (outidx > sizeof(outbuf)) {
+			printf("outidx too big!\n");
+			exit(EXIT_FAILURE);
+		}
+		wavidx = 0;
+	}
+
+	if (rc < 0)
+		perror("wav read");
+	else {
+		while (wavidx < rc) {
+			outbuf[outidx++] = wavbuf[wavidx];
+			wavidx += 4;
+		}
+		if (write(outfd, outbuf, outidx) != outidx)
+				perror("pcm write");
+		else if (outidx > sizeof(outbuf)) {
+			printf("outidx too big!\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	close(infd);
+	close(outfd);
+
+	return 0;
+}
